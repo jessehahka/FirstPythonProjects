27533917efc01952f80d4a9c2461c84f686c5247
jessehahka <67300308+jessehahka@users.noreply.github.com>
2020-07-07 09:27:54 -0700

    Add files via upload

---
+++ Age_In_One_Year.py
@@ -0,0 +1,8 @@
+print("Hello, there!")
+print("What is your name?")    #ask for user's name
+myName = input()
+print("It is good to meet you, " + myName)
+
+print("What is your age?")      #ask for their age
+myAge = input()
+print("You will be " + str(int(myAge) + 1) + " in one year.")
